b83ee70b18bd26dc03baf2a3381d9f9e8411925f
iJustWantTooSee <viktor.skachkov.tomsk@mail.ru>
2021-06-02 20:07:22 +0700

    Добавил стилизацию при создании фильма
    Добавил в js'е функцию для предпросмотра загружаемого изображения

--- FilmsCatalog/wwwroot/js/site.js
+++ FilmsCatalog/wwwroot/js/site.js
@@ -1,4 +1,24 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function handleFileSelect(event) {
+    var selectedFile = event.target.files; // FileList object
+    var file = selectedFile[0];
+    // Only process image files.
+    if (!file.type.match('image.*')) {
+        alert("Image only please");
+    }
+    var reader = new FileReader();
+    // Closure to capture the file information.
+    reader.onload = (function (theFile) {
+        return function (e) {
+            // Render thumbnail.
+            var locationPreview = document.getElementById('preview');
+            var newImage = document.createElement('img');
+            locationPreview.innerHTML = '';
+            newImage.src = e.target.result;
+            newImage.setAttribute("style", "width: 100%; max-width:400px;");
+            locationPreview.appendChild(newImage);
 
-// Write your JavaScript code.
+        };
+    })(file);
+    // Read in the image file as a data URL.
+    reader.readAsDataURL(file);
+}
