b7c66d6faa2252c7050c9804fd8a2dcf985e97e9
iJustWantTooSee <viktor.skachkov.tomsk@mail.ru>
2021-06-02 13:12:42 +0700

    Добавил модели для создания/редактирования Фильма

---
+++ FilmsCatalog/Models/ViewModels/EditFilmViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmsCatalog.Models.ViewModels
+{
+    public class EditFilmViewModel : FilmViewModel
+    {
+        public String PathToPoster { get; set; }
+    }
+}
---
+++ FilmsCatalog/Models/ViewModels/FilmViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmsCatalog.Models.ViewModels
+{
+    public class FilmViewModel
+    {
+        [Required(ErrorMessage = "Enter the name of the film")]
+        public String Name { get; set; }
+
+        [Required(ErrorMessage = "Enter a description of the film")]
+        public String Description { get; set; }
+
+        [Required(ErrorMessage = "Enter the release year of the film")]
+        public DateTime YearsOfRealease { get; set; }
+
+        [Required(ErrorMessage = "Enter the film director")]
+        public String Director { get; set; }
+
+        public IFormFile Poster { get; set; }
+    }
+}
