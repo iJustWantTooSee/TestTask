7eb9ef72df17c076375d4826bcbff02727f29f3c
iJustWantTooSee <viktor.skachkov.tomsk@mail.ru>
2021-06-03 18:19:53 +0700

    Добавил описание к функциям

--- FilmsCatalog/Services/IDirectoryFilesServices.cs
+++ FilmsCatalog/Services/IDirectoryFilesServices.cs
@@ -8,14 +8,40 @@ namespace FilmsCatalog.Services
 {
     public interface IDirectoryFilesServices
     {
+        /// <summary>
+        /// Checks the file extension. Is the file extension valid for uploading a file to the server.
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns>Returns true if the file has a valid extension. otherwise returns false</returns>
         Boolean IsAllowedFileFormat(IFormFile file);
 
+        /// <summary>
+        /// Returns the file extension
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns>File extension in the form of a string in lowercase letters. Format ".extension"</returns>
         String GetFileExtension(IFormFile file);
 
+        /// <summary>
+        /// Generates a new name for the file
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns>A string with a new file name</returns>
         String GetNewFileName(IFormFile file);
 
+        /// <summary>
+        /// Uploads the file to the specified directory on the server under the specified file name. If the specified directory does not exist, it is created on the server.
+        /// </summary>
+        /// <param name="file">Downloadable file.</param>
+        /// <param name="localDirectory">Local path to the directory on the server.</param>
+        /// <param name="fileName">The name of the file under which it will be saved.</param>
+        /// <returns></returns>
         Task AddFileToServer(IFormFile file, String localDirectory ,String  fileName);
 
+        /// <summary>
+        /// Deletes the file from at the specified link. Returns true if the file was deleted and false if the file was not found at the specified path
+        /// </summary>
+        /// <param name="localPathToFile">Local file path</param>
         Boolean DeleteFile(String localPathToFile);
 
     }
--- FilmsCatalog/Services/IUserPermissionsService.cs
+++ FilmsCatalog/Services/IUserPermissionsService.cs
@@ -8,6 +8,11 @@ namespace FilmsCatalog.Services
 {
     public interface IUserPermissionsService
     {
+        /// <summary>
+        /// Returns information about whether the current user can edit the film
+        /// </summary>
+        /// <param name="film"></param>
+        /// <returns>Returns true if the current user can edit the film and false if user cannot edit the film.</returns>
         Boolean IsCanEditFilm(Film film);
     }
 }
