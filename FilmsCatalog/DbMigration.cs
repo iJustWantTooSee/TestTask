af70dcbd6f5baa0ff23910f1b7e9dc21bf7d8e40
iJustWantTooSee <viktor.skachkov.tomsk@mail.ru>
2021-06-03 15:46:39 +0700

    Добавил роль администратора в систему
    Т.к. редактировать фильмы может только тот юзер, кто выложил фильм, то в системе необходим администратор, который будет удалять неприемлемый контент

---
+++ FilmsCatalog/DbMigration.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading.Tasks;
+using FilmsCatalog.Data;
+using FilmsCatalog.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FilmsCatalog
+{
+    public static class DbMigration
+    {
+        public static IHost MigrateDatabase(this IHost webHost)
+        {
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+                context.Database.Migrate();
+                DbMigration.ConfigureIdentity(scope).GetAwaiter().GetResult();
+            }
+
+            return webHost;
+        }
+
+        private static async Task ConfigureIdentity(IServiceScope scope)
+        {
+            var roleManager = scope.ServiceProvider.GetService<RoleManager<IdentityRole>>();
+            var userManager = scope.ServiceProvider.GetService<UserManager<User>>();
+
+            var adminsRole = await roleManager.FindByNameAsync(ApplicationRoles.Administrators);
+            if (adminsRole == null)
+            {
+                var roleResult = await roleManager.CreateAsync(new IdentityRole(ApplicationRoles.Administrators));
+                if (!roleResult.Succeeded)
+                {
+                    throw new InvalidOperationException($"Unable to create {ApplicationRoles.Administrators} role.");
+                }
+
+                adminsRole = await roleManager.FindByNameAsync(ApplicationRoles.Administrators);
+            }
+
+            var adminUser = await userManager.FindByNameAsync("adminFilmsCatalog@localhost.local");
+            if (adminUser == null)
+            {
+                var userResult = await userManager.CreateAsync(new User
+                {
+                    UserName = "adminFilmsCatalog@localhost.local",
+                    Email = "adminFilmsCatalog@localhost.local"
+                }, "AdminPass123!");
+                if (!userResult.Succeeded)
+                {
+                    throw new InvalidOperationException($"Unable to create adminFilmsCatalog@localhost.local user");
+                }
+
+                adminUser = await userManager.FindByNameAsync("adminFilmsCatalog@localhost.local");
+            }
+
+            if (!await userManager.IsInRoleAsync(adminUser, adminsRole.Name))
+            {
+                await userManager.AddToRoleAsync(adminUser, adminsRole.Name);
+            }
+        }
+
+    }
+}
---
+++ FilmsCatalog/Models/ApplicationRoles.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace FilmsCatalog.Models
+{
+    public static class ApplicationRoles
+    {
+        public const String Administrators = nameof(ApplicationRoles.Administrators);
+    }
+}
