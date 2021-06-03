03c6063e240beeca46540253f5a4720320d50c1f
iJustWantTooSee <viktor.skachkov.tomsk@mail.ru>
2021-06-03 18:22:31 +0700

    Добавил описание к константам

--- FilmsCatalog/Constans/FilmsPaginationConstans.cs
+++ FilmsCatalog/Constans/FilmsPaginationConstans.cs
@@ -4,7 +4,14 @@ namespace FilmsCatalog.Constans
 {
     public static class FilmsPaginationConstans
     {
+        /// <summary>
+        /// Number of items displayed on a single page
+        /// </summary>
         public const Int32 PAGE_SIZE = 8;
+
+        /// <summary>
+        /// Number of pages displayed during pagination
+        /// </summary>
         public const Int32 MAX_NUMBER_PAGES = 5;
     }
 }
--- FilmsCatalog/Constans/LocalPathsToDirectories.cs
+++ FilmsCatalog/Constans/LocalPathsToDirectories.cs
@@ -4,6 +4,9 @@ namespace FilmsCatalog.Constans
 {
     public static class LocalPathsToDirectories
     {
+        /// <summary>
+        /// Place to upload posters on the server
+        /// </summary>
         public const String PATH_TO_POSTERS = "images/posters";
     }
 }
